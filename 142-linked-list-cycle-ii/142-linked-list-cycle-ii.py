bb51af63ad9ba16ae29b985795e8229c982cb485
Ahmed Khaled <67319455+AhmedKhaled27@users.noreply.github.com>
2022-09-17 00:16:58 +0200

    Time: 136 ms (9.47%), Space: 17.3 MB (94.29%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -4,18 +4,59 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+#         nodesSet = set()
+#         while head :
+#             if(head in nodesSet):
+#                 return head 
+#             else :
+#                 nodesSet.add(head)
+            
+#             head = head.next
+                
+#         return None
+    
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        nodesSet = set()
-        while head :
-            if(head in nodesSet):
-                return head 
-            else :
-                nodesSet.add(head)
             
-            head = head.next
+        fast = head 
+        slow = head
+        
+        curr = None
+        cycleLenght = 0
+        
+        while fast and fast.next :
+            
+            slow = slow.next 
+            fast = fast.next.next
+            
+            if (fast == slow) :
                 
+                curr = slow
+                slow = slow.next 
+                cycleLenght = 1
+                
+                while curr != slow :
+                    cycleLenght += 1
+                    slow = slow.next
+                    
+                p1 = head 
+                p2 = head 
+                
+                for i in range(cycleLenght) :
+                    p1 = p1.next
+                
+                while p1 :
+                    if p1 == p2 :
+                        return p1 
+                    
+                    p1 = p1.next 
+                    p2 = p2.next
+        
         return None
+                
+    
             
         
